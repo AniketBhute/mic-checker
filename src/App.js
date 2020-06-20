65841645965aa0ea02a22b20119a71cd925b1647
ab403w <ab403w@att.com>
2020-06-20 18:59:25 +0530

    mic checker commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AudioAnalyser from './components/AudioAnalyser';
+import ParticlesBg from "particles-bg";
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null
+    };
+
+    this.toggleMicrophone = this.toggleMicrophone.bind(this);
+
+  }
+
+  async getMicrophone() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+
+  stopMicrophone() {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+
+  toggleMicrophone() {
+    if (this.state.audio) {
+      this.stopMicrophone();
+    } else {
+      this.getMicrophone();
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" color="#31495B" bg={true} />
+        
+        <div className="controls">
+          <button className="micButton" onClick={this.toggleMicrophone}  >
+          {/* {this.state.audio ? 'Stop microphone' : 'Get microphone input'} */}
+          <img src="https://img.icons8.com/flat_round/64/000000/microphone--v1.png"/>
+          </button>
+        </div>
+        {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ''}
+      </div>
+    );
+
+
+  }
 }
 
 export default App;
